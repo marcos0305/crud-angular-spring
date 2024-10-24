642a1519ea6c19a3540ec6e5530e5aa2cff8f19e
marcos0305 <carvalho.oliveira0305@gmail.com>
2024-10-24 12:11:13 -0300

    CRUD Angular + Spring | 04 - Criando o Módulo de Cursos e Usando Roteamento com Lazy Loading

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    AppRoutingModule
   ]
 })
 export class AppModule { }

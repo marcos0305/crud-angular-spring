8a4524c37db6afd06671608f7aeb76f90297aca7
marcos0305 <carvalho.oliveira0305@gmail.com>
2025-02-07 10:45:50 -0300

    CRUD Angular + Spring | 29: Atualizar Curso: Salvando o Formulário

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,5 +1,5 @@
+import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
-import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
 import { delay, first, Observable, tap } from 'rxjs';
 
@@ -26,5 +26,20 @@ export class CoursesService {
       delay(7500),
       tap(courses => console.log(courses))
     );
+  }
+  save1(record: Partial<Course>){
+    if(record._id){
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>){
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>){
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+
   }
 }

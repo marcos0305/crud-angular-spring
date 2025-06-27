b11f83cc432cf533bdb6614c218af6c9e203b6ff
marcos0305 <carvalho.oliveira0305@gmail.com>
2025-06-27 10:18:48 -0300

    CRUD Angular + Spring | 48: Curso-Aulas: Interface Aulas

--- crud-angular/src/app/courses/guards/course.resolver.ts
+++ crud-angular/src/app/courses/guards/course.resolver.ts
@@ -11,5 +11,5 @@ export const courseResolver: ResolveFn<Course> = (route: ActivatedRouteSnapshot,
     return service.loadById(route.params['id']);
   }
 
-  return of({ _id: '', name: '', category: '' });
+  return of({ _id: '', name: '', category: '', lessons: [] });
 };
